b1fca3d76059a0335cbad6a1b1236e024a181b8a
Douwe den Blanken <douwedb@gmail.com>
2019-12-23 17:34:45 +0100

    Updates

---
+++ precache-manifest.9100c0e7449c99f995b2d35ad7b2a084.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67bb60d1d239f1088a1465655db6dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "244c17783ab357d612ce",
+    "url": "/static/css/2.abfbd6db.chunk.css"
+  },
+  {
+    "revision": "01ec0a5acb1046105a54",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "244c17783ab357d612ce",
+    "url": "/static/js/2.0edd4696.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.0edd4696.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01ec0a5acb1046105a54",
+    "url": "/static/js/main.b43c660d.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "20db57ba32a046dfea3c30519898b278",
+    "url": "/static/media/Symbola.20db57ba.svg"
+  },
+  {
+    "revision": "4621fcfd9def63c694914f7ec5add610",
+    "url": "/static/media/Symbola.4621fcfd.otf"
+  },
+  {
+    "revision": "52a6aac18ae26b6ecbd4f3a0d9579c9f",
+    "url": "/static/media/Symbola.52a6aac1.ttf"
+  },
+  {
+    "revision": "b1445a46ceac48f13cec0860ab1acf5f",
+    "url": "/static/media/Symbola.b1445a46.woff"
+  },
+  {
+    "revision": "cb8d804a242b86175fdd6cb8e11b1a35",
+    "url": "/static/media/Symbola.cb8d804a.woff2"
+  },
+  {
+    "revision": "e4ae9ff7ac2476ae421fc4278e5d3806",
+    "url": "/static/media/Symbola.e4ae9ff7.eot"
+  }
+]);

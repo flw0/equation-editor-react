88fbc35769f3273f9d22fd4eed86761576f6a222
Douwe den Blanken <douwedb@gmail.com>
2020-04-09 16:47:13 +0200

    Moved demo into demo folder

---
+++ demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Page from "./components/Page";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Page />
+    </div>
+  );
+};
+
+export default App;
